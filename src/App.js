727e191de8f9fd00a63ca816c42f606ea80c1a34
coffeeandpouch <sameinsidler@me.com>
2021-01-29 14:56:04 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class App extends React.component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+}
+
+render;
+
+return (
+  <div className="medhead">
+    <header>Everyday Medical Dictionary</header>
+  </div>
+);
+ReactDOM.render(element, document.getElementById("root"));
+
+export default App;
