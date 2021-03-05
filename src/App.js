7e88faf53ce7801811d1db95d3cfb75684283bb0
coffeeandpouch <sameinsidler@me.com>
2021-03-05 15:28:22 -0600

    commits changes

--- src/App.js
+++ src/App.js
@@ -3,26 +3,19 @@ import ReactDOM from "react-dom";
 import searchForm from "./searchForm";
 
 class App extends React.component {
-  constructor(props) {
-    super(props);
-    this.state = {};
+  render() {
+    return (
+      <html>
+        <div className="medhead">
+          <h1>Everyday Medical Dictionary</h1>
+        </div>
+        <div>
+          <searchForm />
+        </div>
+      </html>
+    );
+    // eslint-disable-next-line no-unreachable
+    ReactDOM.render(<App />, document.getElementById("root"));
   }
 }
-render() {
-  searchForm ;
-}
-
-
-return (
-  <html>
-    <div className="medhead">
-      <h1>Everyday Medical Dictionary</h1>
-    </div>
-    <div>
-      <searchForm />
-    </div>
-  </html>
-);
-ReactDOM.render(element, document.getElementById("root"));
-
 export default App;
