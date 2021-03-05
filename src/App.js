cba5d0921eee0b6fd29acbe18cbfa64811c70881
coffeeandpouch <sameinsidler@me.com>
2021-03-05 15:33:36 -0600

    commits changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import searchForm from "./searchForm";
+import searchForm from "./searchForm/searchForm";
 
 class App extends React.component {
   render() {
